77001ad6fa97d35868c59281eea8c1485358dd0f
Constantin Dohotaru <dohotaru@milon.com>
2020-08-19 16:31:55 +0200

    Some code cleanup

--- server/src/loaders/index.js
+++ server/src/loaders/index.js
@@ -1,12 +1,8 @@
 import expressLoader from './express';
-import dependencyInjectorLoader from './dependencyInjector';
 import Logger from './logger';
 
 export default async (expressApp) => {
 
-    dependencyInjectorLoader();
-    Logger.info('✌️ Dependency Injector loaded');
-
-    await expressLoader(expressApp);
+    expressLoader(expressApp);
     Logger.info('✌️ Express loaded');
 };
